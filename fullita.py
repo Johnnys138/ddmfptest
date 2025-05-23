1493ec8a682cf4078f437c7024bab05803933a4e
Johnnys138 <dw5003@libero.it>
2025-05-23 18:43:49 +0200

    Update fullita.py

--- fullita.py
+++ fullita.py
@@ -11,8 +11,8 @@ from bs4 import BeautifulSoup
 import time
 
 
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+MFPLINK = "https://simonphil-streamtest.hf.space"     # non mettere lo / finale al link
+MFPPSW = "011"
 
 # Constants
 REFERER = "forcedtoplay.xyz"
