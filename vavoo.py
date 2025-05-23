f6959437e032a06cc5cdf7d04e93231766236be2
Johnnys138 <dw5003@libero.it>
2025-05-23 18:48:28 +0200

    Update vavoo.py

--- vavoo.py
+++ vavoo.py
@@ -3,10 +3,10 @@ import json
 import re
 import os
 
-USREPG = ""
-BRANCHEPG = ""
-MFPLINK = ""     # non mettere lo / finale al link
-MFPPSW = ""
+USREPG = "Johnnys138"
+BRANCHEPG = "ddmfptest"
+MFPLINK = "https://simonphil-streamtest.hf.space"     # non mettere lo / finale al link
+MFPPSW = "011"
 
 PROXY = f"{MFPLINK}/proxy/hls/manifest.m3u8?api_password={MFPPSW}&d="
 HEADER = "&h_user-agent=VAVOO/2.6&h_referer=https://vavoo.to/"
